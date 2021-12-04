64f8d0bc5e3549b22892bc8ca3f077d2d7ad012c
Fedgyma <fedgyma@gmail.com>
2021-12-04 19:14:35 +0100

    fix

--- day_01/d01_first_part.c
+++ day_01/d01_first_part.c
@@ -13,20 +13,23 @@ int main(void)
 
   getline_from_file();
   prev_measurement = atoi(lineptr);
-  printf("%d (N/A - no previous measurement)\n", atoi(lineptr));
+  printf("%d (N/A - no previous measurement)\n", prev_measurement);
 
   while ((getline_from_file()) != -1) {
     curr_measurement = atoi(lineptr);
-    if (curr_measurement > prev_measurement)
+    if (curr_measurement > prev_measurement) {
       measurement_that_are_larger_that_the_prev_measurement_count++;
-      printf("%d (increased)\n", atoi(lineptr));
+      printf("%d (increased)\n", curr_measurement);
+    }
+
     if (curr_measurement < prev_measurement)
-      printf("%d (decreased)\n", atoi(lineptr));
+      printf("%d (decreased)\n", curr_measurement);
+
     prev_measurement = curr_measurement;
   }
 
   clean();
-  
+
   printf("%d measurements are larger than the previous measurement\n", measurement_that_are_larger_that_the_prev_measurement_count);
 
   return EXIT_SUCCESS;
--- day_01/d01_second_part.c
+++ day_01/d01_second_part.c
@@ -9,17 +9,17 @@ int main(void)
   int prev_sum;
   int curr_sum;
   int sums_larger_than_the_previous_sum = 0;
-
   open_file("input_second_part");
 
   /* Aknowledge the first window */
-  for (int i = 0;i < 2; i++)
+  for (int i = 0;i < 3; i++)
   {
     getline_from_file();
     window[i] = atoi(lineptr);
   }
 
   prev_sum = window[0] + window[1] + window[2];
+  printf("%d (N/A - no previous measurement)\n", prev_sum);
   window[0] = window[1]; window[1] = window [2]; // a, b, c => b, c, c
 
   while ((getline_from_file()) != -1)
@@ -27,8 +27,16 @@ int main(void)
     window[2] = atoi(lineptr);
     curr_sum = window[0] + window[1] + window[2];
 
-    if (curr_sum > prev_sum)
+    if (curr_sum > prev_sum) {
       sums_larger_than_the_previous_sum++;
+      printf("%d (increased)\n", curr_sum);
+    }
+
+    if (curr_sum < prev_sum)
+      printf("%d (decreased)\n", curr_sum);
+
+    if (curr_sum == prev_sum)
+      printf("%d (no change)\n", curr_sum);
 
     prev_sum = curr_sum;
     window[0] = window[1]; window[1] = window [2];
