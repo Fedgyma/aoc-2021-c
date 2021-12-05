1136dc72d58fa383ee573b2c78d125b5e11ac365
Fedgyma <fedgyma@gmail.com>
2021-12-05 23:32:39 +0100

    filebase

---
+++ day_03/d03_second_part.c
@@ -0,0 +1,19 @@
+#include "aoc.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(void)
+{
+  open_file("test");
+
+  while ((getline_from_file()) != -1)
+  {
+
+  }
+
+  clean();
+
+  return EXIT_SUCCESS;
+}
