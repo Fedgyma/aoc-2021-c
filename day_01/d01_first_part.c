3f5912c80fa67f0a315182c3187412a48c2d951d
Fedgyma <fedgyma@gmail.com>
2021-12-06 00:27:48 +0100

    removed aoc.h for clarity

--- day_01/d01_first_part.c
+++ day_01/d01_first_part.c
@@ -1,21 +1,33 @@
-#include "aoc.h"
+#define _GNU_SOURCE
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
+
+  FILE * fp;
+  char * lineptr = NULL;
+  size_t n = 0;
+  ssize_t n_char_read;
+
   int prev_measurement;
   int curr_measurement;
   int measurement_that_are_larger_that_the_prev_measurement_count = 0;
 
-  open_file("puzzle_input");
+  fp = fopen((PUZZLE_INPUT), "r"));
+  if (fp == NULL)
+  {
+    perror("Error opening file " (PUZZLE_INPUT));
+    exit(EXIT_FAILURE);
+  }
 
-  getline_from_file();
+  n_char_read = getline(&lineptr, &n, fp);
   prev_measurement = atoi(lineptr);
   printf("%d (N/A - no previous measurement)\n", prev_measurement);
 
-  while ((getline_from_file()) != -1) {
+  while ((n_char_read = getline(&lineptr, &n, fp)) != -1) {
     curr_measurement = atoi(lineptr);
     if (curr_measurement > prev_measurement) {
       measurement_that_are_larger_that_the_prev_measurement_count++;
@@ -28,9 +40,14 @@ int main(void)
     prev_measurement = curr_measurement;
   }
 
-  clean();
+  fclose(fp);
+  if (lineptr)
+  {
+    free(lineptr);
+  }
 
-  printf("%d measurements are larger than the previous measurement\n", measurement_that_are_larger_that_the_prev_measurement_count);
+  printf("%d measurements are larger than the previous measurement\n",
+                measurement_that_are_larger_that_the_prev_measurement_count);
 
   return EXIT_SUCCESS;
 }
